2d4f7e4074ed228f2bc3ba986292c31d738cb01f
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2026-01-08 15:50:29 +0000

    Added a couple of tests for input in the console.

--- tests/playwright/e2e/console-execution.spec.ts
+++ tests/playwright/e2e/console-execution.spec.ts
@@ -41,12 +41,17 @@ async function checkConsoleContent(page: Page, expectedContent : string) {
     expect(actual).toEqual(expectedContent);
 }
 
-async function runToFinish(page: Page) {
+async function startRunning(page: Page) {
     // It should not be running:
     const button = page.locator("#runButton");
     await expect(button).toHaveText("Run");
     // Click it:
     await page.click("#runButton");
+    return button;
+}
+
+async function runToFinish(page: Page) {
+    const button = await startRunning(page);
     // Then it should not be running again, because it has finished:
     await expect(button).toHaveText("Run");
 }
@@ -78,5 +83,33 @@ test.describe("Check console after execution", () => {
         await page.click("#runButton");
         await runToFinish(page);
         await checkConsoleContent(page, "Line 1\nLine 2\nLine 3.0\\nLine 3.1\n");
+    });
+});
+
+test.describe("Test stdin works", () => {
+    test("Check input/output works", async ({page}) => {
+        await enterCode(page, ["", "", "name = input('What is your name?\\n')\nprint('Hello ' + name)\n"]);
+        const button = await startRunning(page);
+        await expect(page.locator("#peaConsole")).toBeEnabled();
+        await expect(page.locator("#peaConsole")).toBeFocused();
+        await page.locator("#peaConsole").pressSequentially("George\n", {delay: 75});
+        // Then it should not be running again, because it has finished:
+        await expect(button).toHaveText("Run");
+        await checkConsoleContent(page, "What is your name?\nGeorge\nHello George\n");
+    });
+
+    test("Check multiple input works", async ({page}) => {
+        await enterCode(page, ["", "", "name = input('What is your name?\\n')\nprint('Hello ' + name)\nspecies = input('What is your species?\\n')\nprint('Hello ' + name + ' the ' + species)\n"]);
+        const button = await startRunning(page);
+        await expect(page.locator("#peaConsole")).toBeEnabled();
+        await expect(page.locator("#peaConsole")).toBeFocused();
+        await page.locator("#peaConsole").pressSequentially("George\n", {delay: 75});
+        await checkConsoleContent(page, "What is your name?\nGeorge\nHello George\nWhat is your species?\n");
+        // Should still be running:
+        await expect(button).toContainText("Stop");
+        await page.locator("#peaConsole").pressSequentially("cat\n", {delay: 75});
+        await checkConsoleContent(page, "What is your name?\nGeorge\nHello George\nWhat is your species?\ncat\nHello George the cat\n");
+        // Then it should not be running again, because it has finished:
+        await expect(button).toHaveText("Run");
     });
 });

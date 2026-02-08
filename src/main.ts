32007b554925f7cc48b01f652e0fed7162b4dfc8
calebchanwy <calebchanwy@gmail.com>
2026-02-08 17:19:38 +0000

    Update frame IDs for tutorial and project documentation
    Addresses: #5

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ export const AppPlatform = appPlatform;
 
 // The project defintion slot isn't attached to a "real" frame.
 // We declare the fake frame ID we used for it here.
-export const projectDocumentationFrameId = -10;
+export const tutorialFrameId = -10;
+export const projectDocumentationFrameId = -9;
+
 
 let localeBuildDate = "";
 export function getLocaleBuildDate(): string {

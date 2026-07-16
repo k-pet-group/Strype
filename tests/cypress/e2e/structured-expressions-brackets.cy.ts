e446e42526aee279dee6c9fcda4aa2b7119a85f5
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-07-16 10:09:50 +0100

    Fix the for test  for microbit (media one still faulty)

--- tests/cypress/e2e/structured-expressions-brackets.cy.ts
+++ tests/cypress/e2e/structured-expressions-brackets.cy.ts
@@ -25,9 +25,9 @@ describe("Test brackets", () => {
 
     it("Test brackets in a for frame LHS", () => {
         focusEditor();
-        cy.get("body").type("f");
-        cy.get("body").type("a,(b,c)");
-        assertState(3, "a,(b,c)$");
+        cy.get("body").type("f").then(() => {
+            cy.get("body").type("a,(b,c)").then(() => assertState((Cypress.env("mode") == "microbit") ? 4 : 3, "a,(b,c)$"));
+        });
     });
 });
 
